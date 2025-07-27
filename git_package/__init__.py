7a2ed000e0ffbc3d043d284777069526b99eb8df
Antonio V <antoniov77v@gmail.com>
2025-07-27 11:12:18 +0700

    feat: extend path

--- git_package/__init__.py
+++ git_package/__init__.py
@@ -1,4 +1,8 @@
+import sys
+from pathlib import Path
+
 from .src.start_dev import start_dev
 
-# from .src.app import start_app
-# from .src.db.db import init_db
+sys.path.append((Path(__file__).parent / "src").as_posix())
+
+__all__ = ["start_dev"]
